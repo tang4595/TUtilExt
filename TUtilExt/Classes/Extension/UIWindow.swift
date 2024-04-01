dfd6a429647f8cc596c9ed566dd2ddb0cbd53c60
tang4595 <137059428@qq.com>
2024-04-01 15:54:48 +0800

    feature

---
+++ TUtilExt/Classes/Extension/UIWindow.swift
@@ -0,0 +1,35 @@
+//
+//  UIWindow.swift
+//  TUtilExt
+//
+//  Created by tang on 2024/4/1.
+//
+
+import UIKit
+
+public extension UIWindow {
+    static var activeWindow: UIWindow? {
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .sorted { $0.activationState.sortPriority < $1.activationState.sortPriority }
+                .compactMap { $0 as? UIWindowScene }
+                .compactMap { $0.windows.first { $0.isKeyWindow } }
+                .first
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+private extension UIScene.ActivationState {
+    var sortPriority: Int {
+        switch self {
+        case .foregroundActive: return 1
+        case .foregroundInactive: return 2
+        case .background: return 3
+        case .unattached: return 4
+        @unknown default: return 5
+        }
+    }
+}
